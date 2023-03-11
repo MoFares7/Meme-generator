ac4fd6bb17184ce380b884790d24d283cbf5895c
MoFares7 <faresdabbas1@gmail.com>
2023-03-11 18:51:54 +0200

    add body to sit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Meme from './components/Body/Meme';
 import Header from './components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
