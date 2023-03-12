31a1863a3dc8b99d11a0fb1644a7b4512c9ab994
MoFares7 <faresdabbas1@gmail.com>
2023-03-12 19:53:05 +0200

    add State in our Components

--- src/components/Body/Meme.js
+++ src/components/Body/Meme.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "./Meme.css"
+import memesData from "../Data/MemeData"
 
 export default function Meme() {
+
+       // const [memeImage, setMemeImage] = React.useState("");
+        const [meme, setMeme] = React.useState({
+                topText: "",
+                bottomText: "",
+                randomImage: "http://i.imgflip.com/1bij.jpg"
+        })
+        
+        const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+        function getMemeImage()
+        {
+                const memesArray = memesData.data.memes;
+                const randomNumber = Math.floor(Math.random() * memesArray.length)
+                const url = memesArray[randomNumber].url
+                setMeme(prevImage => ({
+                        ...prevImage,
+                        randomImage: url
+                }))   
+        }
+
+
         return (
                 <main>
-                        <form className="form">
+                        <div className="form">
                                 <input
                                         className="form--input"
                                         type="text"
@@ -17,10 +40,12 @@ export default function Meme() {
                                 />
                                 <button
                                         className="form--button"
+                                        onClick={getMemeImage}
                                 >
                                         Get a new meme image 🖼
                                 </button>
-                        </form>
+                        </div>
+                        <img className="meme--image" src={meme.randomImage} />
                 </main>
         )
 }
